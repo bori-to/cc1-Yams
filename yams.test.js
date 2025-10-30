1c3876f6c0c94737c9a10103ab56b242cf941af1
Adrien <adrien0878@gmail.com>
2025-10-30 10:43:57 +0100

    ajout de tests

--- yams.test.js
+++ yams.test.js
@@ -27,4 +27,19 @@ describe("Score du YAMS", () => {
     it("Chance = somme des 5 dés", () => {
         expect(scoreRoll([1, 2, 3, 4, 6])).toBe(16);
     });
+
+    it("Ne détecte pas de full si 4 dés identiques", () => {
+        expect(scoreRoll([3, 3, 3, 3, 5])).toBe(35);
+    });
+
+    it("Ne détecte pas de brelan avec seulement une paire", () => {
+        expect(scoreRoll([2, 2, 5, 6, 1])).toBe(16);
+    });
+
+    it("prend bien la meilleure figure", () => {
+        expect(scoreRoll([5, 5, 5, 5, 5])).toBe(50);
+        expect(scoreRoll([2, 2, 2, 2, 2])).toBe(50);
+        expect(scoreRoll([4, 4, 4, 4, 1])).toBe(35);
+        expect(scoreRoll([6, 6, 6, 2, 2])).toBe(30);
+    });
 });
